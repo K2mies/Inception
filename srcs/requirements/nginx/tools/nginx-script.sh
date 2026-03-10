401a0992226e7d00fbd39b3ab2a145fe0b2483dd
Ross Hvidsten <Rosshvidsten@googlemail.com>
2026-03-10 13:06:24 +0000

    most parts are put together now except for the Makefile

---
+++ srcs/requirements/nginx/tools/nginx-script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir	-p	/etc/nginx/ssl
+
+openssl	req	-x509 -nodes -days 365		\
+-newkey		rsa:2048			\
+-keyout		/etc/nginx/ssl/inception.key	\
+-out		/etc/nginx/ssl/inception.crt	\
+-subj		"/C=FI/ST=Uusimaa/L=Helsinki/O=42/OU=42/CN=$DOMAIN_NAME"
+
+nginx	-g	"daemon off";
