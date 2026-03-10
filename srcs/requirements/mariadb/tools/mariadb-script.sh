17b6a7fd496758f4648950b3b15c51f07db18e95
Ross Hvidsten <Rosshvidsten@googlemail.com>
2026-03-10 13:21:43 +0000

    some small fixes

--- srcs/requirements/mariadb/tools/mariadb-script.sh
+++ srcs/requirements/mariadb/tools/mariadb-script.sh
@@ -3,21 +3,23 @@
 echo "==> Setting up MariaDB directory..."
 chmod -R 755 /var/lib/mysql
 
-mkdir -p /run/mysql
-
-chown -R mysql:mysql /var/lib/mysql /run/mysql
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	echo "==> Initializing MariaDB system tables"
 	mariadb-install-db --basedir=/usr --user=mysql --datadir=/var/lib/mysql >/dev/null
 
 	echo "==> Creating WordPress database and user..."
-	mysql --user=mysql --bootstrap << EOF
+	mysqld --user=mysql --bootstrap << EOF
+
+
 USE mysql
 FLUSH PRIVILEGES
 ALTER USER 'root'@'localhost' IDENTIFIED BY "${MYSQL_ROOT_PASSWORD}";
-CREATE DATABASE ${WORDPRESS_DATABASE_NAME} CHARACTER SET utf8 COLLATE utf8_genral_ci;
-CREATE USER '${WORDPRESS_DATABASE_USER}'@'%' IDENTIFIED BY '${WORDPRESS_DATABASE_PASSWORD}';
+CREATE DATABASE ${WORDPRESS_DATABASE_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${WORDPRESS_DATABASE_USER}'@'%' IDENTIFIED BY '${WORDPRESS_DATABASE_USER_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${WORDPRESS_DATABASE_NAME}.* TO '${WORDPRESS_DATABASE_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
